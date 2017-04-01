b5907f39d2d5b137051bd140fc3174f03971b554
Herb Peyerl <hpeyerl+gitlab@beer.org>
2017-04-01 12:28:12 +0000

    <tab> vs <spaces> snuck in

--- pi_mqtt_gpio/modules/raspberrypi.py
+++ pi_mqtt_gpio/modules/raspberrypi.py
@@ -36,7 +36,7 @@ class GPIO(GenericGPIO):
         else:
             pullup = PULLUPS[pullup]
 
-	self.io.setup(pin, direction, pull_up_down=pullup)
+        self.io.setup(pin, direction, pull_up_down=pullup)
 
     def set_pin(self, pin, value):
         self.io.output(pin, value)
