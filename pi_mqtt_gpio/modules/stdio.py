ff33bae9a89ad12ac116ecf5b04106f67f289ee3
Ellis Percival <flyte@failcode.co.uk>
2017-07-14 11:56:45 +0100

    Use enum34 to enable installation on Python 3

--- pi_mqtt_gpio/modules/stdio.py
+++ pi_mqtt_gpio/modules/stdio.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from pi_mqtt_gpio.modules import GenericGPIO
 
 
@@ -6,15 +8,15 @@ class GPIO(GenericGPIO):
     Implementation of GPIO class for outputting to STDIO.
     """
     def __init__(self, config):
-        print "__init__(config=%r)" % config
+        print("__init__(config=%r)" % config)
 
     def setup_pin(self, pin, direction, pullup, pin_config):
-        print "setup_pin(pin=%r, direction=%r, pullup=%r, pin_config=%r)" % (
-            pin, direction, pullup, pin_config)
+        print("setup_pin(pin=%r, direction=%r, pullup=%r, pin_config=%r)" % (
+            pin, direction, pullup, pin_config))
 
     def set_pin(self, pin, value):
-        print "set_pin(pin=%r, value=%r)" % (pin, value)
+        print("set_pin(pin=%r, value=%r)" % (pin, value))
 
     def get_pin(self, pin):
-        print "get_pin(pin=%r)" % pin
+        print("get_pin(pin=%r)" % pin)
         return False
