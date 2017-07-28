f7439edbb0e53eb43def39d1e6cfaf71417218cf
Ellis Percival <flyte@failcode.co.uk>
2017-07-28 18:54:28 +0100

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class SchemaCommand(Command):
 
 setup(
     name="pi_mqtt_gpio",
-    version="0.0.16",
+    version="0.1.0",
     cmdclass={"insert_schema": SchemaCommand},
     packages=find_packages(
         exclude=[
