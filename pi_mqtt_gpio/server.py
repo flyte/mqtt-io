54dc9e41dd48e5d0d8aaaf2ca8cefb8cc50749c2
ComlOnline <ComlOnline@users.noreply.github.com>
2017-11-22 22:54:49 +0000

    Retain works as it should

--- pi_mqtt_gpio/server.py
+++ pi_mqtt_gpio/server.py
@@ -127,7 +127,8 @@ def set_pin(output_config, value):
     payload = output_config["on_payload" if value else "off_payload"]
 	retain = output_config["retain"]
     client.publish(
-        "%s/%s/%s" % (topic_prefix, OUTPUT_TOPIC, output_config["name"]), retain=retain,
+        "%s/%s/%s" % (topic_prefix, OUTPUT_TOPIC, output_config["name"]),
+		retain=retain,
         payload=payload)
 
 
