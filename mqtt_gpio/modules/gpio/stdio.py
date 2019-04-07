5d7c730f7ab0f6682e45fc55e9bd9d0a7ec8bbea
Ellis Percival <flyte@failcode.co.uk>
2019-04-07 18:40:32 +0100

    Add output loops for configured outputs. Wire up output modules.

--- mqtt_gpio/modules/gpio/stdio.py
+++ mqtt_gpio/modules/gpio/stdio.py
@@ -23,8 +23,14 @@ class GPIO(GenericGPIO):
             elif initial == "low":
                 self.set_pin(pin, False)
 
+    async def async_set_pin(self, pin, value):
+        self.set_pin(pin, value)
+
     def set_pin(self, pin, value):
         print("set_pin(pin=%r, value=%r)" % (pin, value))
+
+    async def async_get_pin(self, pin):
+        return self.get_pin(pin)
 
     def get_pin(self, pin):
         print("get_pin(pin=%r)" % pin)
--- mqtt_gpio/server.py
+++ mqtt_gpio/server.py
@@ -1,6 +1,6 @@
 import asyncio
 import re
-import signal
+import logging
 from functools import partial
 from importlib import import_module
 
@@ -9,6 +9,11 @@ from .config import validate_and_normalise_config
 from .modules import BASE_SCHEMA as MODULE_BASE_SCHEMA
 from .modules import install_missing_requirements
 from .modules.gpio import PinDirection, PinPUD
+
+_LOG = logging.getLogger(__name__)
+
+SET_TOPIC = "set"
+OUTPUT_TOPIC = "output"
 
 MODULE_CLASS_NAMES = dict(gpio="GPIO", sensor="Sensor")
 
@@ -49,9 +54,13 @@ class MqttGpio:
     def __init__(self, config):
         self.config = config
         self.gpio_configs = {}
+        self.digital_input_configs = {}
+        self.digital_output_configs = {}
         self.gpio_modules = {}
+        self.module_output_queues = {}
 
         self.loop = asyncio.get_event_loop()
+        self.tasks = []
 
         self._init_gpio_modules()
         self._init_digital_inputs()
@@ -66,6 +75,7 @@ class MqttGpio:
             )
 
     def _init_digital_inputs(self):
+        self.digital_input_configs = {x["name"]: x for x in self.config["digital_inputs"]}
         # IDEA: Possible implementations -@flyte at 07/04/2019, 15:34:39
         # Could this be done asynchronously?
         for in_conf in self.config["digital_inputs"]:
@@ -79,16 +89,37 @@ class MqttGpio:
             )
 
     def _init_digital_outputs(self):
+        self.digital_output_configs = {
+            x["name"]: x for x in self.config["digital_outputs"]
+        }
         # IDEA: Possible implementations -@flyte at 07/04/2019, 15:34:54
         # Could this be done asynchronously?
         for out_conf in self.config["digital_outputs"]:
             self.gpio_modules[out_conf["module"]].setup_pin(
                 out_conf["pin"], PinDirection.OUTPUT, None, out_conf
             )
+            # Create queues for each module with an output
+            if out_conf["module"] not in self.module_output_queues:
+                queue = asyncio.Queue()
+                self.module_output_queues[out_conf["module"]] = queue
+
+                # Create loops which handle new entries on the queue
+                async def output_loop():
+                    while True:
+                        module, module_config, output_config, payload = await queue.get()
+                        pin = output_config["pin"]
+                        if payload == output_config["on_payload"]:
+                            await module.async_set_pin(pin, True)
+                        elif payload == output_config["off_payload"]:
+                            await module.async_set_pin(pin, False)
+                        else:
+                            print("Payload was weird: %r" % payload)
+
+                self.tasks.append(self.loop.create_task(output_loop()))
 
     async def _init_mqtt(self):
         self.mqtt = await mqtt.connect(self.config["mqtt"]["host"])
-        await self.mqtt.subscribe([("mqtt_gpio/input/test", mqtt.QOS_1)])
+        await self.mqtt.subscribe([("mqtt_gpio/#", mqtt.QOS_1)])
 
     async def _mqtt_loop(self):
         try:
@@ -97,28 +128,39 @@ class MqttGpio:
                 topic = msg.publish_packet.variable_header.topic_name
                 payload = msg.publish_packet.payload.data.decode("utf8")
                 print("%s - %s" % (topic, payload))
+                _LOG.info("Received message on topic %r: %r", topic, payload)
+                if topic.endswith("/%s" % SET_TOPIC):
+                    topic_prefix = self.config["mqtt"]["topic_prefix"]
+                    lindex = len("%s/%s/" % (topic_prefix, OUTPUT_TOPIC))
+                    rindex = -len(SET_TOPIC) - 1
+                    output_name = topic[lindex:rindex]
+                    output_config = self.digital_output_configs[output_name]
+                    module_config = self.gpio_configs[output_config["module"]]
+                    module = self.gpio_modules[output_config["module"]]
+                    queue = self.module_output_queues[module_config["name"]]
+                    queue.put_nowait((module, module_config, output_config, payload))
         finally:
             print("\nDisconnecting from MQTT...")
             await self.mqtt.disconnect()
             print("MQTT disconnected")
 
     def run(self):
-        async def say_hi_loop():
-            while True:
-                print("Hi!")
-                await asyncio.sleep(1)
-
         self.loop.run_until_complete(self._init_mqtt())
-        tasks = asyncio.gather(self._mqtt_loop(), say_hi_loop())
+        tasks = asyncio.gather(self._mqtt_loop())
         try:
             self.loop.run_until_complete(tasks)
         except KeyboardInterrupt:
             tasks.cancel()
-            for task in self.mqtt.client_tasks:
+            for task in list(self.mqtt.client_tasks) + self.tasks:
                 task.cancel()
             self.loop.run_until_complete(
                 asyncio.gather(
-                    *([tasks] + list(self.mqtt.client_tasks)), return_exceptions=True
+                    *(self.tasks + [tasks] + list(self.mqtt.client_tasks)),
+                    return_exceptions=True
                 )
             )
 
+    # async def output_loop(self):
+    #     while True:
+    #         item = await self.module_output_queues["stdio"].get()
+    #         print("Got item! %r" % (item,))
