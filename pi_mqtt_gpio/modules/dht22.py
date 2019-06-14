0efbd8de8801c0db17ee5da14e55dbad1a38118f
Malte <46646495+SkySails@users.noreply.github.com>
2019-06-14 15:36:19 +0200

    Corrected lowercase module reference (#66) Fixes #65

--- pi_mqtt_gpio/modules/dht22.py
+++ pi_mqtt_gpio/modules/dht22.py
@@ -32,7 +32,7 @@ class Sensor(GenericSensor):
         if sensor_type == "dht22":
             self.sensor_type = DHTsensor.DHT22
         elif sensor_type == "dht11":
-            self.sensor_type = DHTsensor.dht11
+            self.sensor_type = DHTsensor.DHT11
         elif sensor_type == "am2302":
             self.sensor_type = DHTsensor.AM2302
         else:
