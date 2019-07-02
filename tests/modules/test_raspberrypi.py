a736a8ddb30ac93bc8a28e01fda02c15c28eec8a
Benji <46675043+BenjiU@users.noreply.github.com>
2019-07-02 09:46:32 +0200

    Updating comments in module tests

--- tests/modules/test_raspberrypi.py
+++ tests/modules/test_raspberrypi.py
@@ -2,17 +2,20 @@ import pytest
 import time
 from pi_mqtt_gpio.modules import raspberrypi, PinDirection, PinPullup, InterruptEdge
 
+
 pytestmark = pytest.mark.hw_raspberrypi
 
 '''
-Definition of wired connected GPIOs
-You may connect them with a 10kOhm resistor
+Attention: 
+The following tests will only work with hardware modifikations.
+Definition of wired connected GPIOs, always konnect xxx_OUTPUT
+with xxx_INPUT. You may connect them with a 10kOhm resistor.
 '''
-# Pins for get and set
+# gpio pins for get and set
 TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT = 26
 TEST_RASPBERRYPI_GPIO_SET_GET_INPUT  = 21
 
-# Pins for get and set
+# gpio pins interrupt tests
 TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_OUTPUT  = 19
 TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT   = 20
 TEST_RASPBERRYPI_GPIO_INTERRUPT_FALLING_OUTPUT = 13
@@ -20,59 +23,51 @@ TEST_RASPBERRYPI_GPIO_INTERRUPT_FALLING_INPUT  = 16
 TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_OUTPUT    = 6
 TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_INPUT     = 12
 
-#global gpio and interrupt callback function
-gpio = 0
+# global gpio and interrupt callback function
+gpio = null
 interrupt_count = 0
 test_handle = 0
 
+# Use this function for each testcase
 @pytest.fixture(autouse=True)
 def fix_raspberrypi_setup_teardown():
-    # Code that will run before your test, for example:
+    # initialize gpios and counter for interrupt before each test case
     global gpio
     gpio = raspberrypi.GPIO("")
     global interrupt_count
     interrupt_count = 0
 
-    # A test function will be run at this point
+    # run the test case here
     yield
 
-    # Code that will run after your test, for example:
+    # clean up the gpios after each test
     gpio.cleanup()
 
 def gpio_testcallback(handle, pin, value):
     '''
-    callback function for test interrupts
-    '''
-    global interrupt_count
-    interrupt_count = interrupt_count + 1
-    # debug output: 
-    #print("callback: %s" % datetime.datetime.now().time())
-
-def gpio_testcallback_handle(handle, pin, value):
-    '''
-    callback function for test interrupts
+    callback function for test interrupts, count the calls of this function
+    and store a given handle.
     '''
     global interrupt_count
     interrupt_count = interrupt_count + 1
     global test_handle
     test_handle = handle
-    # debug output: 
-    #print("callback: %s" % datetime.datetime.now().time())
 
 def test_raspberrypi_setup_output_pin():
-    # setup gpio TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT as output
+    # setup a output gpio
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT, PinDirection.OUTPUT, None, {'initial' : None})
 
 def test_raspberrypi_setup_input_pin():
-    # setup gpio TEST_RASPBERRYPI_GPIO_SET_GET_INPUT as input without pullup
+    # setup a input gpio
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_INPUT, PinDirection.INPUT, PinPullup.OFF, {})
 
 def test_raspberrypi_set_pin():
-    # set the value of pin TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT high
+    # setup a output gpio and set value to high
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT, PinDirection.OUTPUT, None, {'initial' : None})
     gpio.set_pin(TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT, True)
     
 def test_raspberrypi_get_pin():
+    # setup a output and input gpio and read the value from input
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT, PinDirection.OUTPUT, None, {'initial' : None})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_INPUT, PinDirection.INPUT, PinPullup.OFF, {})
     # get the value of the connected input
@@ -80,6 +75,7 @@ def test_raspberrypi_get_pin():
     assert value == True
 
 def test_raspberrypi_low_high():
+    # setup a output gpio and set value to low and high
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT, PinDirection.OUTPUT, None, {'initial' : 'high'})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_SET_GET_INPUT, PinDirection.INPUT, PinPullup.OFF, {})
     # set gpio TEST_RASPBERRYPI_GPIO_SET_GET_OUTPUT low
@@ -96,11 +92,11 @@ def test_raspberrypi_low_high():
     assert value == False
 
 def test_raspberrypi_interrupt_callback_handle():
-    # setup outputs and inputs for interrupt tests for rising edges
+    # test for handle passing
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_OUTPUT, PinDirection.OUTPUT, None, {'initial' : "low"})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT, PinDirection.INPUT, PinPullup.UP, {})
     gpio.io.remove_event_detect(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT)
-    gpio.setup_interrupt("myhandle", TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT, InterruptEdge.RISING, gpio_testcallback_handle, 300)
+    gpio.setup_interrupt("myhandle", TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT, InterruptEdge.RISING, gpio_testcallback, 300)
 
     gpio.set_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_OUTPUT, True)  # one rising edge
     time.sleep(0.1)  # wait to callback function happens
@@ -119,7 +115,7 @@ def test_raspberrypi_interrupt_trigger_rising_single():
     assert interrupt_count == 1
 
 def test_raspberrypi_interrupt_trigger_rising_multi():
-    # setup outputs and inputs for interrupt tests for rising edges
+    # setup outputs and inputs for interrupt tests for multiple rising edges
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_OUTPUT, PinDirection.OUTPUT, None, {'initial' : "low"})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT, PinDirection.INPUT, PinPullup.UP, {})
     gpio.io.remove_event_detect(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT)
@@ -137,7 +133,7 @@ def test_raspberrypi_interrupt_trigger_rising_multi():
     assert interrupt_count == 3
     
 def test_raspberrypi_interrupt_trigger_rising_bouncetime():
-    # setup outputs and inputs for interrupt tests for rising edges
+    # test for bouncetime, miss one interrupt durring bounce time
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_OUTPUT, PinDirection.OUTPUT, None, {'initial' : "low"})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT, PinDirection.INPUT, PinPullup.UP, {})
     gpio.io.remove_event_detect(TEST_RASPBERRYPI_GPIO_INTERRUPT_RISING_INPUT)
@@ -179,7 +175,7 @@ def test_raspberrypi_interrupt_trigger_falling_single():
     assert interrupt_count == 1 # should not happen on rising edge
     
 def test_raspberrypi_interrupt_trigger_both_single():
-    # setup outputs and inputs for interrupt tests for both edges
+    # test for rising and falling edge configuration
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_OUTPUT, PinDirection.OUTPUT, None, {'initial' : "low"})
     gpio.setup_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_INPUT, PinDirection.INPUT, PinPullup.OFF, {})
     gpio.io.remove_event_detect(TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_INPUT)
@@ -192,19 +188,3 @@ def test_raspberrypi_interrupt_trigger_both_single():
     gpio.set_pin(TEST_RASPBERRYPI_GPIO_INTERRUPT_BOTH_OUTPUT, False)  # set output low
     time.sleep(0.2)  # wait to callback function happens
     assert interrupt_count == 2  #  interrupt on falling edge
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
