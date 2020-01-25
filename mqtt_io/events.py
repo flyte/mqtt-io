8752858d6c8bba373f5d3b987ba652740856edf3
Ellis Percival <flyte@failcode.co.uk>
2020-01-25 15:56:23 +0000

    Better log messages in events.py

--- mqtt_io/events.py
+++ mqtt_io/events.py
@@ -14,36 +14,37 @@ class EventBus:
         self._listeners = {}
 
     def fire(self, event):
+        event_class = type(event)
         try:
-            listeners = self._listeners[type(event)]
+            listeners = self._listeners[event_class]
             _LOG.debug(
-                "Found %s listener(s) for event type %r", len(listeners), type(event)
+                "Found %s listener(s) for event type %s",
+                len(listeners),
+                event_class.__name__,
             )
         except KeyError:
-            _LOG.debug("No listeners for event type %r", type(event))
+            _LOG.debug("No listeners for event type %s", event_class.__name__)
             return
 
         for listener in listeners:
             self._loop.create_task(listener(event))
 
-    def subscribe(self, event_type, callback):
-        if not isinstance(event_type, type):
+    def subscribe(self, event_class, callback):
+        if not isinstance(event_class, type):
             raise TypeError(
-                "event_type must be of type 'type'. Got type %r." % type(event_type)
+                "event_class must be of type 'type'. Got type %s." % type(event_class)
             )
         if not isinstance(callback, types.FunctionType):
             raise TypeError(
-                "callback must be of type 'types.FunctionType'. Got type %r."
+                "callback must be of type 'types.FunctionType'. Got type %s."
                 % type(callback)
             )
-        if event_type in self._listeners:
-            self._listeners[event_type].append(callback)
+        if event_class in self._listeners:
+            self._listeners[event_class].append(callback)
         else:
-            self._listeners[event_type] = [callback]
-
-        _LOG.debug(self._listeners)
+            self._listeners[event_class] = [callback]
 
         def remove_listener():
-            self._listeners[event_type].remove(callback)
+            self._listeners[event_class].remove(callback)
 
         return remove_listener
