466f0af23f05b930ac56fb4f0b317b230b07205a
Ellis Percival <flyte@failcode.co.uk>
2020-01-25 15:24:36 +0000

    Remove some debut logs

--- mqtt_io/io.py
+++ mqtt_io/io.py
@@ -23,11 +23,9 @@ async def digital_input_poller(event_bus, module, input_config):
     while True:
         value = await module.async_get_pin(input_config["pin"])
         if value != last_value:
-            _LOG.debug("Firing event...")
             event_bus.fire(
                 DigitalInputChangedEvent(input_config["name"], last_value, value)
             )
-            _LOG.debug("After event fired.")
             last_value = value
         # TODO: Tasks pending completion -@flyte at 29/05/2019, 01:02:50
         # Make this delay configurable in input_config
