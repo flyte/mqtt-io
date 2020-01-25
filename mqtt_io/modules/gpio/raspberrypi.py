497a4103ef948f8280aa90d0570eb43696bbb51e
Ellis Percival <flyte@failcode.co.uk>
2020-01-25 02:24:14 +0000

    Add raspberry pi module

---
+++ mqtt_io/modules/gpio/raspberrypi.py
@@ -0,0 +1,48 @@
+from pi_mqtt_gpio.modules import GenericGPIO, PinDirection, PinPullup
+
+
+REQUIREMENTS = ("RPi.GPIO",)
+
+DIRECTIONS = None
+PULLUPS = None
+
+
+class GPIO(GenericGPIO):
+    """
+    Implementation of GPIO class for Raspberry Pi native GPIO.
+    """
+
+    def __init__(self, config):
+        global DIRECTIONS, PULLUPS
+        import RPi.GPIO as gpio
+
+        self.io = gpio
+        DIRECTIONS = {PinDirection.INPUT: gpio.IN, PinDirection.OUTPUT: gpio.OUT}
+
+        PULLUPS = {
+            PinPullup.OFF: gpio.PUD_OFF,
+            PinPullup.UP: gpio.PUD_UP,
+            PinPullup.DOWN: gpio.PUD_DOWN,
+        }
+
+        gpio.setmode(gpio.BCM)
+
+    def setup_pin(self, pin, direction, pullup, pin_config):
+        direction = DIRECTIONS[direction]
+
+        if pullup is None:
+            pullup = PULLUPS[PinPullup.OFF]
+        else:
+            pullup = PULLUPS[pullup]
+
+        initial = {None: -1, "low": 0, "high": 1}[pin_config.get("initial")]
+        self.io.setup(pin, direction, pull_up_down=pullup, initial=initial)
+
+    def set_pin(self, pin, value):
+        self.io.output(pin, value)
+
+    def get_pin(self, pin):
+        return self.io.input(pin)
+
+    def cleanup(self):
+        self.io.cleanup()
