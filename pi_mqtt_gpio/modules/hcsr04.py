4ce11322c37816467558a817a37b1f464f12d1b0
Ellis Percival <flyte@failcode.co.uk>
2020-03-04 16:39:28 +0000

    Formatting changes, avoid 'sum' naming clash.

--- pi_mqtt_gpio/modules/hcsr04.py
+++ pi_mqtt_gpio/modules/hcsr04.py
@@ -21,28 +21,30 @@ PULSE = 0.00001
 # sonic speed/2
 SPEED_2 = 17015
 
-class Sensor(GenericSensor):
 
+class Sensor(GenericSensor):
     def __init__(self, config):
         import RPi.GPIO as GPIO
 
         self.gpio = GPIO
-        self.pin_Trigger = config["pin_trigger"]
-        self.pin_Echo = config["pin_echo"]
+        self.pin_trigger = config["pin_trigger"]
+        self.pin_echo = config["pin_echo"]
         self.burst = config["burst"]
 
     def setup_sensor(self, config):
         # use BCM GPIO-references (instead of Pin-numbers)
         # and define GPIO-input/output
         self.gpio.setmode(self.gpio.BCM)
-        self.gpio.setup(self.pin_Trigger,self.gpio.OUT)
-        self.gpio.setup(self.pin_Echo,self.gpio.IN)
-        self.gpio.remove_event_detect(self.pin_Echo)
-        self.gpio.output(self.pin_Trigger, False)
-        time.sleep(1)                           # Setup-time for Sensor
-        self.gpio.add_event_detect(self.pin_Echo, self.gpio.BOTH, callback=self.measure)    # create callback triggered by rising and falling edge
+        self.gpio.setup(self.pin_trigger, self.gpio.OUT)
+        self.gpio.setup(self.pin_echo, self.gpio.IN)
+        self.gpio.remove_event_detect(self.pin_echo)
+        self.gpio.output(self.pin_trigger, False)
+        # Setup-time for Sensor
+        time.sleep(1)
+        # create callback triggered by rising and falling edge
+        self.gpio.add_event_detect(self.pin_echo, self.gpio.BOTH, callback=self.measure)
 
-        self.stopp = 0
+        self.stop = 0
         self.start = 0
         self.distance = 0
 
@@ -52,32 +54,50 @@ class Sensor(GenericSensor):
         value = self.measure_range()
         return value
 
-    def pulse(self):                            # function to start measurement
-      self.gpio.output(self.pin_Trigger, True)  # create trigger pulse
-      time.sleep(PULSE)
-      self.gpio.output(self.pin_Trigger, False)
-      self.stopp = 0
-      self.start = 0
-      self.distance = 0
+    def pulse(self):
+        """
+        Starts measurement
+        """
+        # create trigger pulse
+        self.gpio.output(self.pin_trigger, True)
+        time.sleep(PULSE)
+        self.gpio.output(self.pin_trigger, False)
+        self.stop = 0
+        self.start = 0
+        self.distance = 0
 
-    def measure(self, x):                       # Callback-Function for echo signal
-      if self.gpio.input(self.pin_Echo) == 1:   # save time of rising echo signal
-        self.start = time.time()
-      else:                                     # falling edge, save time
-        self.stopp = time.time()
-        delta = self.stopp - self.start         # calculate time difference sending / receiving
-        self.distance = delta * SPEED_2         # calculate distance by time and sonic speed
+    def measure(self, x):
+        """
+        Callback function for echo signal
+        """
+        if self.gpio.input(self.pin_echo) == 1:
+            # save time of rising echo signal
+            self.start = time.time()
+        else:
+            # falling edge, save time
+            self.stop = time.time()
+            # calculate time difference sending / receiving
+            delta = self.stop - self.start
+            # calculate distance by time and sonic speed
+            self.distance = delta * SPEED_2
 
-    def measure_range(self):                    # start measures and calculate average of BURST measurements
-      values = []
-      sum = 0
-      for i in range(0, self.burst):
-        self.pulse()                            # start measurement
-        time.sleep(0.040)                       # wait till end of measurement
-        values.append(self.distance)            # save value in array and sum up
-        sum = sum + values[i]
-        time.sleep(0.05)
-      return sum/self.burst;                         # return average
+    def measure_range(self):
+        """
+        Start measures and calculate average of BURST measurements
+        """
+        values = []
+        ranges_sum = 0
+        for i in range(0, self.burst):
+            # start measurement
+            self.pulse()
+            # wait till end of measurement
+            time.sleep(0.040)
+            # save value in array and sum up
+            values.append(self.distance)
+            ranges_sum += values[i]
+            time.sleep(0.05)
+        # return average
+        return ranges_sum / self.burst
 
     def cleanup(self):
         self.gpio.cleanup()
