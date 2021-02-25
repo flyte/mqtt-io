a33bb5c2de9620d6834f008a0c9442c70bed08fe
Rene Lehfeld <rlehfeld@gmx.de>
2021-02-25 19:10:01 +0100

    port feature/optional_display_name_for_homeassistant_discovery to feature/asyncio and change it according to the thread conversation in the closed merge request

--- mqtt_io/home_assistant.py
+++ mqtt_io/home_assistant.py
@@ -16,24 +16,31 @@ _LOG = logging.getLogger(__name__)
 
 
 def get_common_config(
-    mqtt_config: ConfigType, mqtt_options: MQTTClientOptions
+    dev_config: ConfigType, mqtt_config: ConfigType, mqtt_options: MQTTClientOptions
 ) -> Dict[str, Any]:
     """
     Return config that's common across all HQ discovery announcements.
     """
-    return dict(
-        availability_topic="/".join(
-            (mqtt_config["topic_prefix"], mqtt_config["status_topic"])
-        ),
-        payload_available=mqtt_config["status_payload_running"],
-        payload_not_available=mqtt_config["status_payload_dead"],
-        device=dict(
-            manufacturer="MQTT IO",
-            identifiers=["mqtt-io", mqtt_options.client_id],
-            name=mqtt_config["discovery_name"],
-        ),
-    )
+    config = {}
 
+    if "ha_discovery" in dev_config:
+        config.update(dev_config["ha_discovery"])
+
+    config.update(
+        dict(
+            availability_topic="/".join(
+                (mqtt_config["topic_prefix"], mqtt_config["status_topic"])
+            ),
+            payload_available=mqtt_config["status_payload_running"],
+            payload_not_available=mqtt_config["status_payload_dead"],
+            device=dict(
+                manufacturer="MQTT IO",
+                identifiers=["mqtt-io", mqtt_options.client_id],
+                name=mqtt_config["discovery_name"],
+            ),
+        )
+    )
+    return config
 
 def hass_announce_digital_input(
     in_conf: ConfigType, mqtt_config: ConfigType, mqtt_options: MQTTClientOptions
@@ -43,10 +50,10 @@ def hass_announce_digital_input(
     """
     name: str = in_conf["name"]
     disco_prefix: str = mqtt_config["discovery_prefix"]
-    sensor_config = get_common_config(mqtt_config, mqtt_options)
+    sensor_config = get_common_config(in_conf, mqtt_config, mqtt_options)
     sensor_config.update(
         dict(
-            name=name,
+            name=sensor_config.pop("display_name", name),
             unique_id=f"{mqtt_options.client_id}_{in_conf['module']}_input_{name}",
             state_topic="/".join((mqtt_config["topic_prefix"], INPUT_TOPIC, name)),
             payload_on=in_conf["on_payload"],
@@ -54,7 +61,7 @@ def hass_announce_digital_input(
         )
     )
     return MQTTMessageSend(
-        "/".join((disco_prefix, "binary_sensor", mqtt_options.client_id, name, "config")),
+        "/".join((disco_prefix, sensor_config.pop("component", "binary_sensor"), mqtt_options.client_id, name, "config")),
         json.dumps(sensor_config).encode("utf8"),
         retain=True,
     )
@@ -71,10 +78,10 @@ def hass_announce_digital_output(
     name: str = out_conf["name"]
     prefix: str = mqtt_config["topic_prefix"]
     disco_prefix: str = mqtt_config["discovery_prefix"]
-    switch_config = get_common_config(mqtt_config, mqtt_options)
+    switch_config = get_common_config(out_conf, mqtt_config, mqtt_options)
     switch_config.update(
         dict(
-            name=name,
+            name=switch_config.pop("display_name", name),
             unique_id=f"{mqtt_options.client_id}_{out_conf['module']}_output_{name}",
             state_topic="/".join((prefix, OUTPUT_TOPIC, name)),
             command_topic="/".join((prefix, OUTPUT_TOPIC, name, SET_SUFFIX)),
@@ -83,7 +90,7 @@ def hass_announce_digital_output(
         )
     )
     return MQTTMessageSend(
-        "/".join((disco_prefix, "switch", mqtt_options.client_id, name, "config")),
+        "/".join((disco_prefix, switch_config.pop("component", "switch"), mqtt_options.client_id, name, "config")),
         json.dumps(switch_config).encode("utf8"),
         retain=True,
     )
@@ -101,10 +108,10 @@ def hass_announce_sensor_input(
     prefix: str = mqtt_config["topic_prefix"]
     disco_prefix: str = mqtt_config["discovery_prefix"]
     expire_after: int = sens_conf.get("expire_after", sens_conf["interval"] * 2 + 5)
-    sensor_config = get_common_config(mqtt_config, mqtt_options)
+    sensor_config = get_common_config(sens_conf, mqtt_config, mqtt_options)
     sensor_config.update(
         dict(
-            name=name,
+            name=sensor_config.pop("display_name", name),
             unique_id=f"{mqtt_options.client_id}_{sens_conf['module']}_sensor_{name}",
             state_topic="/".join((prefix, SENSOR_TOPIC, name)),
             expire_after=expire_after,
@@ -113,7 +120,7 @@ def hass_announce_sensor_input(
     if "unit_of_measurement" in sens_conf:
         sensor_config["unit_of_measurement"] = sens_conf["unit_of_measurement"]
     return MQTTMessageSend(
-        "/".join((disco_prefix, "sensor", mqtt_options.client_id, name, "config")),
+        "/".join((disco_prefix, sensor_config.pop("component", "sensor"), mqtt_options.client_id, name, "config")),
         json.dumps(sensor_config).encode("utf8"),
         retain=True,
     )
