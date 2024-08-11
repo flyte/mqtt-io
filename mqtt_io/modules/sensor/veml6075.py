337bd8a5a1343ef15fa8694f19e530a0c75bdbca
Guy Martin <gmsoft@tuxicoman.be>
2024-08-11 13:39:41 +0100

    Don't turn off the sensor and don't use high dynamic range. Also get rid of alpha-gamma values.

--- mqtt_io/modules/sensor/veml6075.py
+++ mqtt_io/modules/sensor/veml6075.py
@@ -5,6 +5,7 @@ VEML 6075 UV sensor
 from mqtt_io.types import ConfigType, SensorValueType
 from . import GenericSensor
 import logging
+import time
 
 REQUIREMENTS = ("smbus2", "veml6075",)
 
@@ -38,10 +39,6 @@ class Sensor(GenericSensor):
     """
 
     SENSOR_SCHEMA = {
-        "alpha": {"type": "float", "required": False, "empty": False, "default": 1.0},
-        "beta": {"type": "float", "required": False, "empty": False, "default": 1.0},
-        "gamma": {"type": "float", "required": False, "empty": False, "default": 1.0},
-        "delta": {"type": "float", "required": False, "empty": False, "default": 1.0},
         "a": {"type": "float", "required": False, "empty": False, "default": 2.22},
         "b": {"type": "float", "required": False, "empty": False, "default": 1.33},
         "c": {"type": "float", "required": False, "empty": False, "default": 2.95},
@@ -57,17 +54,21 @@ class Sensor(GenericSensor):
 
         self.bus = SMBus(self.config["i2c_bus_num"])
         self.sensor = VEML6075(i2c_dev=self.bus)
+        self.sensor.set_shutdown(True)
+        self.sensor.set_high_dynamic_range(False)
+        self.sensor.set_integration_time('100ms')
+        self.sensor.set_shutdown(False)
 
 
     def calculate_uv_index(self, sens_conf, uva, uvb, uv_comp1, uv_comp2) -> float:
-
-        uva_calc = uva - ((sens_conf["a"] * sens_conf["alpha"] * uv_comp1) / sens_conf["gamma"]) - ((sens_conf["b"] * sens_conf["alpha"] * uv_comp2) / sens_conf["delta"])
+        _LOG.info("UVA: " + str(uva) + " UVB: " + str(uvb) + " UV_comp1: " + str(uv_comp1) + " UV_comp2: " + str(uv_comp2))
+        uva_calc = uva - (sens_conf["a"] * uv_comp1) - (sens_conf["b"] * uv_comp2)
         _LOG.info("uva_calc " + str(uva_calc))
-        uvb_calc = uvb - ((sens_conf["c"] * sens_conf["beta"] * uv_comp1) / sens_conf["gamma"]) - ((sens_conf["d"] * sens_conf["beta"] * uv_comp2) / sens_conf["delta"])
+        uvb_calc = uvb - (sens_conf["c"] * uv_comp1) - (sens_conf["d"] * uv_comp2)
         _LOG.info("uvb_calc " + str(uvb_calc))
-        uva_index = uva_calc * (1 / sens_conf["alpha"]) * sens_conf["UVAresp"]
+        uva_index = uva_calc * sens_conf["UVAresp"]
         _LOG.info("uva_index " + str(uva_index))
-        uvb_index = uvb_calc * (1 / sens_conf["beta"]) * sens_conf["UVBresp"]
+        uvb_index = uvb_calc * sens_conf["UVBresp"]
         _LOG.info("uvb_index " + str(uvb_index))
         uv_index = (uva_index + uvb_index) / 2
         return uv_index
@@ -77,16 +78,14 @@ class Sensor(GenericSensor):
         Get the UV index from the sensor
         """
         # Setup and turn on the sensor
-        self.sensor.set_shutdown(False)
-        self.sensor.set_high_dynamic_range(False)
-        self.sensor.set_integration_time('100ms')
 
         # Fetch the values
+        #time.sleep(.2)
         uva, uvb = self.sensor.get_measurements()
         uv_comp1, uv_comp2 = self.sensor.get_comparitor_readings()
 
         # Turn off the sensor
-        self.sensor.set_shutdown(True)
+        #self.sensor.set_shutdown(True)
 
         # Calculate and return the UV index
         return self.calculate_uv_index(sens_conf, uva, uvb, uv_comp1, uv_comp2)
