5bd454c9801a699dea97062862c3a85bccf58877
Benji <46675043+BenjiU@users.noreply.github.com>
2026-03-25 21:57:34 +0100

    Add configurable chip address for SHT4x sensor

--- mqtt_io/modules/sensor/sht4x.py
+++ mqtt_io/modules/sensor/sht4x.py
@@ -10,6 +10,9 @@ from ...exceptions import RuntimeConfigError
 
 REQUIREMENTS = ("adafruit-circuitpython-sht4x",)
 
+CONFIG_SCHEMA = {
+    "chip_addr": {"type": "integer", "required": False, "empty": False, "default": 0x44}
+}
 
 class Sensor(GenericSensor):
     """
@@ -33,7 +36,7 @@ class Sensor(GenericSensor):
         import busio  # type: ignore
 
         i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_sht4x.SHT4x(i2c)
+        self.sensor = adafruit_sht4x.SHT4x(i2c, address=self.config["chip_addr"])
 
     @property
     def _temperature(self) -> SensorValueType:
